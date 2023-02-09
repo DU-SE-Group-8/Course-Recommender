0cb81476da257adf95881d70d53107a04e92d84f
loukel <louis.kelly1@hotmail.com>
2023-02-09 16:56:52 +0000

    Added freetype

--- client/src/components/InputWrapper.jsx
+++ client/src/components/InputWrapper.jsx
@@ -1,6 +1,6 @@
 const InputWrapper = ({children}, _) => {
   return ( 
-    <div className="p-4 py-8 border-t-2 border-base-300 flex justify-center flex-wrap">
+    <div className="p-4 py-8 border-t-2 border-base-300 flex flex-row justify-center flex-wrap">
       {children}
     </div>
   );
--- client/src/components/MultiChoiceInput.jsx
+++ client/src/components/MultiChoiceInput.jsx
@@ -3,15 +3,20 @@ import InputWrapper from "./InputWrapper";
 const MultiChoiceInput = ({ answers=[], submit }) => {
   return ( 
     <InputWrapper>
-      {answers.map((answer, index) => (
-        <button 
-          key={index} 
-          className="btn btn-primary m-2"
-          onClick={() => submit(answer)}
-        >
-          {answer}
-        </button>
-      ))}
+      <div className="w-full flex justify-center">
+        {answers.map((answer, index) => (
+          <button 
+            key={index} 
+            className="btn btn-primary m-2"
+            onClick={() => submit(answer)}
+          >
+            {answer}
+          </button>
+        ))}
+      </div>
+      <div className="w-full flex justify-center">
+        <input type="text" placeholder="Type here" className="input input-bordered input-primary w-full max-w-lg flex items-center m-4" />
+      </div>
     </InputWrapper>
   );
 }
