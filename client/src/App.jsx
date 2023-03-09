62faba56414a2edb2a8f888adb0a4f27aaf88cec
loukel <louis.kelly1@hotmail.com>
2023-03-09 16:29:21 +0000

    Removed overflow

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import Chat from "./Chat"
 
 function App() {
   return (
-    <div className="App font-varela-round flex justify-center bg-neutral">
+    <div className="App font-varela-round flex justify-center bg-neutral overflow-hidden">
       <Chat />
     </div>
   )
