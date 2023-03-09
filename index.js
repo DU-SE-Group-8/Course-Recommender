0459ea9060194903b258851b3b07cfca8204afb9
loukel <louis.kelly1@hotmail.com>
2023-03-09 01:25:07 +0000

    Added optional port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./server')
 
 // Start Server 🎉
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 const server = app.listen(PORT, () => {
   console.log(`CORS-enabled web server listening on ${PORT}`)
 })
